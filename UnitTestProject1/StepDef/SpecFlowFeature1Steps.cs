3ecbd826f267717ae7e133c92aef1af5a77c9e24
Mustang <Mustang@DESKTOP-IP36T60>
2017-07-02 11:43:54 +0100

    added 3 more write line statements

--- UnitTestProject1/StepDef/SpecFlowFeature1Steps.cs
+++ UnitTestProject1/StepDef/SpecFlowFeature1Steps.cs
@@ -10,18 +10,21 @@ namespace UnitTestProject1.StepDef
         public void GivenIHaveEnteredIntoTheCalculator(int p0)
         {
             Console.WriteLine("good bad");
+            Console.WriteLine("good bad --- 1");
         }
         
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
             Console.WriteLine("good bad");
+            Console.WriteLine("good bad --- 1");
         }
         
         [Then(@"the result should be (.*) on the screen")]
         public void ThenTheResultShouldBeOnTheScreen(int p0)
         {
             Console.WriteLine("good bad");
+            Console.WriteLine("good bad --- 1");
         }
     }
 }
